7a2793dc2a8d4b088388fa039c53764710c8d07f
GeorgeU2030 <georgeu2021@gmail.com>
2025-02-07 13:43:54 -0500

    design for the home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatIconModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatSnackBarModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  joinGroupForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.joinGroupForm = this.fb.group({
+      user: ['', [Validators.required, Validators.minLength(3)]],
+      chatGroup: ['', [Validators.required, Validators.minLength(2)]]
+    });
+  }
+
+  joinGroup(): void {
+    if (this.joinGroupForm.valid) {
+      console.log('Joining group', this.joinGroupForm.value);
+    }
+  }
 }
